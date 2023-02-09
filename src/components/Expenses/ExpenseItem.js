51fc608cdeabdda258c25325262ff25252dbfbae
Paweł <pawel_gnat@o2.pl>
2023-02-09 22:31:02 +0100

    chore: use state and form handling

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,21 @@
+import Card from '../UI/Card'
+import ExpenseDate from './ExpenseDate'
+import './ExpenseItem.css'
+
+const ExpenseItem = props => {
+	const month = props.date.toLocaleString('en-US', { month: 'long' })
+	const year = props.date.getFullYear()
+	const day = props.date.toLocaleString('en-US', { day: '2-digit' })
+
+	return (
+		<Card className='expense-item'>
+			<ExpenseDate date={props.date} />
+			<div className='expense-item__description'>
+				<h2>{props.title}</h2>
+				<div className='expense-item__price'>{props.amount}</div>
+			</div>
+		</Card>
+	)
+}
+
+export default ExpenseItem
